980c1b23b70a017d005673947f123f78ccf4747d
MukulSaiPendem <144606414+MukulSaiPendem@users.noreply.github.com>
2024-03-27 12:26:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -127,26 +127,7 @@ def hello_pubsub(cloud_event):
     html_content = f"""
     <html>
         <body>
-            <p>Hi {username},</p>
-            <p>Please verify your email address by clicking on the button below:</p>
-            <table width="100%" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td>
-                        <table cellspacing="0" cellpadding="0">
-                            <tr>
-                                <td style="border-radius: 2px;" bgcolor="#ED2939">
-                                    <a href="{verification_link}" target="_blank" style="padding: 8px 12px; border: 1px solid #ED2939;border-radius: 2px;color: #ffffff; font-family: Helvetica, Arial, sans-serif; font-size: 14px; text-decoration: none; display: inline-block;">
-                                        Verify Email
-                                    </a>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
-            <p>If you did not request this, please ignore this email.</p>
-            <br>
-            <p>If you're having trouble with the button above, copy and paste the URL below into your web browser:</p>
+            <p>Please verify your email address by clicking on the link below:</p>
             <p><a href="{verification_link}">{verification_link}</a></p>
         </body>
     </html>
@@ -159,7 +140,7 @@ def hello_pubsub(cloud_event):
         data={
             "from": f"Webapp <noreply@mail.{domain_name}>",
             "to": email,
-            "subject": "Please Verify Your Email Address",
+            "subject": "Verify Email Address",
             "html": html_content
         }
     )
